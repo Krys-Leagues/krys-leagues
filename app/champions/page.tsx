fbebbfd629a4cc750d031e75500a93afeeb5d9b4
krystal ahern <krystal.ahern83@gmail.com>
2026-07-21 11:21:03 -0400

    Add Hall of Champions page

---
+++ app/champions/page.tsx
@@ -0,0 +1,18 @@
+export default function ChampionsPage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#020617",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 32,
+        fontWeight: 700,
+      }}
+    >
+      Hall of Champions - Coming Soon
+    </main>
+  )
+}
