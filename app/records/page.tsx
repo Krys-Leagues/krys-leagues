9f03902eaa0e25b996deb5c969e3e58758dc4e90
krystal ahern <krystal.ahern83@gmail.com>
2026-07-21 11:17:28 -0400

    Add League Records page

---
+++ app/records/page.tsx
@@ -0,0 +1,18 @@
+export default function RecordsPage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#020617",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 32,
+        fontWeight: 700,
+      }}
+    >
+      League Records - Coming Soon
+    </main>
+  )
+}
