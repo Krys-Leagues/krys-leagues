1f5240467e9e81ce8fcc681054c41cf6ad3d5326
krystal ahern <krystal.ahern83@gmail.com>
2026-07-21 11:14:37 -0400

    Add Player Profiles page

---
+++ app/players/page.tsx
@@ -0,0 +1,18 @@
+export default function PlayersPage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#020617",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 32,
+        fontWeight: 700,
+      }}
+    >
+      Player Profiles - Coming Soon
+    </main>
+  )
+}
