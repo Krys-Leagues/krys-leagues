dbb6450eef392a8e4b387738ef45bf2c23b9df51
krystal ahern <krystal.ahern83@gmail.com>
2026-08-24 17:35:34 -0400

    Add protected KWT website recovery review

---
+++ app/admin/kwt-import/website-recovery/page.tsx
@@ -0,0 +1,5 @@
+import KwtWebsiteRecoveryReview from "@/app/admin/kwt-import/website-recovery/KwtWebsiteRecoveryReview"
+
+export default function KwtWebsiteRecoveryPage() {
+  return <KwtWebsiteRecoveryReview />
+}
